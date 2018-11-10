2aa3eaed7bbd667b26670e9b3cbc2f851d8d4214
Josh <joshisadesigner@gmail.com>
2018-11-09 18:00:03 -0600

    Add favicon.ico file to cache

--- sw.js
+++ sw.js
@@ -4,22 +4,21 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(cacheName)
         .then( cache => {
-            return cache.addAll(
-                [
-                    '/css/styles.css',
-                    '/data/restaurants.json',
-                    '/img/1_320.jpg',
-                    '/img/2_320.jpg',
-                    '/img/3_320.jpg',
-                    '/img/4_320.jpg',
-                    '/img/5_320.jpg',
-                    '/img/6_320.jpg',
-                    '/img/7_320.jpg',
-                    '/img/8_320.jpg',
-                    '/img/9_320.jpg',
-                    '/img/10_320.jpg ',
-                ]
-            )
+            return cache.addAll([
+                "/favicon.ico",
+                "/css/styles.css",
+                "/data/restaurants.json",
+                "/img/1_320.jpg",
+                "/img/2_320.jpg",
+                "/img/3_320.jpg",
+                "/img/4_320.jpg",
+                "/img/5_320.jpg",
+                "/img/6_320.jpg",
+                "/img/7_320.jpg",
+                "/img/8_320.jpg",
+                "/img/9_320.jpg",
+                "/img/10_320.jpg "
+            ]);
         })
     );
 })
