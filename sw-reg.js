3ca53a18b64445ffc0e3783e1c840b0018fe365a
Josh <joshisadesigner@gmail.com>
2018-11-14 09:56:39 -0600

    Fix typo in console log

--- sw-reg.js
+++ sw-reg.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('/sw.js')
             .then(registration => {
-                console.log('Registration successfuel, scope is:', registration.scope);
+                console.log("Registration successful, scope is:", registration.scope);
             })
             .catch(error => {
                 console.log('Service worker registration failed, error:', error);
