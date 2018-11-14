fe5364be8c52a63bb9015ce67825135a70b057e7
Josh <joshisadesigner@gmail.com>
2018-11-14 12:06:47 -0600

    Add install and fetch event

--- sw.js
+++ sw.js
@@ -1,3 +1,35 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('restaurants-v1')
+            .then(cache => {
+                return cache.addAll([
+                    "/",
+                    "/css/styles.css",
+                    "/js/main.js",
+                    "/data/restaurants.json",
+                    "/img/1_320.jpg",
+                    "/img/2_320.jpg",
+                    "/img/3_320.jpg",
+                    "/img/4_320.jpg",
+                    "/img/5_320.jpg",
+                    "/img/6_320.jpg",
+                    "/img/7_320.jpg",
+                    "/img/8_320.jpg",
+                    "/img/9_320.jpg",
+                    "/img/10_320.jpg",
+                    "/index.html",
+                    "/restaurant.html"
+                ]);
+            })
+    );
+})
+
 self.addEventListener('fetch', event => {
-    console.log(event.request);
+    event.respondWith(
+        caches.match(event.request)
+            .then( response => {
+                if (response) return response;
+                return fetch(event.request);
+            })
+    )
 });
